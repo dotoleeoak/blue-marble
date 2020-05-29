59396733652e41a36f11c31583115f93e2fef54a
Lenovo <Lenovo@DESKTOP-CIALRSM>
2020-05-29 10:15:39 +0900

    2020-05-29-10:15

---
+++ src/BlueMarble.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class BlueMarble {
+
+	public static void main(String[] args) {
+		//플레이어 수를 입력 받는 단계
+		int num_player = 0;	//플레이어 수
+		Player[] player_list = new Player[4]; //플레이어 리스트 저장
+		try {
+			//Scanner scan = new Scanner(System.in);
+			do{
+				num_player = scan.nextInt();
+				//만약 numPlayer가 유효범위 내가 아니면 경고창 띄우기 구문 추가하기
+			}while( !(num_player >=1 && num_player <=4) );
+			for(int i = 0; i < num_player; i++) {
+				
+				
+			}
+			
+			
+		}catch(Exception e) {
+			
+		}
+		
+		
+		//플레이어들의 이름을 입력 받는 단계
+
+	}
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,21 @@
 
 public class Player {
+	String name;
 	int id;
 	int have_money;
 	int position; 
 	String[] chance = new String[10];   
-	//int ¿ÏÁÖÈ½¼ö 
+	//int completeNumber
 	int island;
+	Player(int _id, String _name) {
+		name = _name;
+		id = _id;
+		have_money = 2000;
+		position = -1;
+		island = 0;
+		for(int i = 0; i < 10; i++) {
+			chance[i] = "";
+		}
+		// completeNumber = 0;
+	}
 }
