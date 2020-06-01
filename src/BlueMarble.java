1ac0029c407a338872055da4c8cb4495c587c0b5
Jaemin Choi <1dotolee@gmail.com>
2020-06-02 03:29:04 +0900

    make menuGUI excutable (temporary) 임시로 menuGUI 실행창만 띄워둠 다른 GUI 틀 잡히면 수정 예정

--- src/BlueMarble.java
+++ src/BlueMarble.java
@@ -6,37 +6,31 @@ import javax.swing.*;
 
 public class BlueMarble {
 
-	public static final int SCREEM_WIDTH = 800;
+	public static final int SCREEN_WIDTH = 800;
 	public static final int SCREEN_HEIGHT = 600;
-	
-	public static void main(String[] args) {
-		BoardGUI2 a = new BoardGUI2();
 
-		
-		
-		
-		
-		//플레이어 수를 입력 받는 단계
-		int num_player = 0;	//플레이어 수
-		Player[] player_list = new Player[4]; //플레이어 리스트 저장
-		try {
-			Scanner scan = new Scanner(System.in);
-			do{
-				num_player = scan.nextInt();
-				//만약 numPlayer가 유효범위 내가 아니면 경고창 띄우기 구문 추가하기
-			}while( !(num_player >=1 && num_player <=4) );
-			for(int i = 0; i < num_player; i++) {
-				
-				
-			}
-			
-			
-		}catch(Exception e) {
-			
-		}
-		
-		
-		//플레이어들의 이름을 입력 받는 단계
+	public static void main(String[] args) {
+		// BoardGUI a = new BoardGUI();
+		menuGUI GUI = new menuGUI();
+
+		// 플레이어 수를 입력 받는 단계
+		// int num_player = 0; // 플레이어 수
+		// Player[] player_list = new Player[4]; // 플레이어 리스트 저장
+		// try {
+		// Scanner scan = new Scanner(System.in);
+		// do {
+		// num_player = scan.nextInt();
+		// // 만약 numPlayer가 유효범위 내가 아니면 경고창 띄우기 구문 추가하기
+		// } while (!(num_player >= 1 && num_player <= 4));
+		// for (int i = 0; i < num_player; i++) {
+
+		// }
+
+		// } catch (Exception e) {
+
+		// }
+
+		// 플레이어들의 이름을 입력 받는 단계
 
 	}
 
